563dedbaa2e84ebaba860b76cb6c731c7572b653
stfu <138953304+NPS809@users.noreply.github.com>
2024-05-06 20:36:28 +1000

    Updated!
    Added a ability to set num frequency, the higher the number, the higher the frequency.

--- tg.py
+++ tg.py
@@ -3,15 +3,22 @@ from string import ascii_letters
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def generate(length: int, seed: str):
+def generate(length: int, seed: str, num_frequency):
     random.seed(seed)
     token = ""
+    num_frequency = 1 if num_frequency < 1 else num_frequency
     for _ in range(length):
-        if random.randint(1, 2) == 2:
+        if random.randint(0, num_frequency) == 0:
             token += random.choice(ascii_letters)
         else:
             token += str(random.choice(nums))
     return token
 
-print(f"Your token {generate(input("Input token lenght:"), input("Input seed:"))}")
-input()
+
+lenght = 25
+seed = "Hello World!"
+num_freq = 1
+
+token = generate(lenght, seed, num_freq)
+
+input(token)
